67f1f714ea05412a568cbae91252e319737c2a12
breneser <eser.bren@gmail.com>
2017-09-30 15:57:13 +0100

    Add smooth scroll and sticky nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
 (
+
   function init() {
 
+    $(document).ready(function(){
+      // Sticky nav
+      $('.nav-wrapper').sticky({topSpacing:0});
+      // smooth scroll
+      $('.nav-wrapper a').smoothScroll();
+    });
+
   }
+
 )();
