1e2c24d5e1129a80b86484b70c06d6d0b1859101
breneser <eser.bren@gmail.com>
2017-10-06 01:08:44 +0100

    Travis fail on deploy error

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
- find dist/ -exec curl -T {} ftp://"$DEPLOY_HOST"/"$DEPLOY_DIR"/ --user "$DEPLOY_USER":"$DEPLOY_PASS" \;
+shopt -s globstar
+set -e
+
+cd dist && pwd && for f in **/*; do if [ -f "$f" ]; then curl --ftp-create-dirs -T "$f" ftp://"$DEPLOY_HOST"/"$DEPLOY_DIR"/"$f" --user "$DEPLOY_USER":"$DEPLOY_PASS"; fi  done
+
