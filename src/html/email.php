ef3050fc4d94c8838e12f14d410c20f679d05376
breneser <eser.bren@gmail.com>
2017-10-06 01:23:31 +0100

    Fix email headers

--- src/html/email.php
+++ src/html/email.php
@@ -59,8 +59,10 @@ if(isset($_POST['submit'])) {
   $email_message .= "Message: ".clean_string($message)."\n";
 
   // create email headers
-  $headers = 'From: '.$email_from."\r\n".
-    'Reply-To: '.$email_from."\r\n" .
+  $headers = 'MIME-Version: 1.0' . "\r\n";
+  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+  $headers .= 'From:  ' . $name . ' <' . $email_from .'>' . " \r\n" .
+    'Reply-To: '.  $email_from . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
   @mail($email_to, $email_subject, $email_message, $headers);
 
