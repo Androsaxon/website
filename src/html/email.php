440fc82c82a0d8516674a7daa488b8ce385ed0d5
breneser <eser.bren@gmail.com>
2017-10-06 01:39:19 +0100

    Revert all email changes

--- src/html/email.php
+++ src/html/email.php
@@ -59,7 +59,7 @@ if(isset($_POST['submit'])) {
   $email_message .= "Message: ".clean_string($message)."\n";
 
   // create email headers
-  $headers = 'From: webmaster@androsaxon.co.uk'."\r\n".
+  $headers = 'From: '.$email_from."\r\n".
     'Reply-To: '.$email_from."\r\n" .
     'X-Mailer: PHP/' . phpversion();
   @mail($email_to, $email_subject, $email_message, $headers);
