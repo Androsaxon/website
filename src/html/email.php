4fee2b02ac9d4451cd4ca010d9012d97cb34697d
breneser <eser.bren@gmail.com>
2017-10-06 01:35:32 +0100

    Revert email send changes

--- src/html/email.php
+++ src/html/email.php
@@ -59,10 +59,8 @@ if(isset($_POST['submit'])) {
   $email_message .= "Message: ".clean_string($message)."\n";
 
   // create email headers
-  $headers = 'MIME-Version: 1.0' . "\r\n";
-  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-  $headers .= 'From:  ' . $name . ' <webmaster@androsaxon.co.uk>' . " \r\n" .
-    'Reply-To: '.  $email_from . "\r\n" .
+  $headers = 'From: webmaster@androsaxon.co.uk'."\r\n".
+    'Reply-To: '.$email_from."\r\n" .
     'X-Mailer: PHP/' . phpversion();
   @mail($email_to, $email_subject, $email_message, $headers);
 
